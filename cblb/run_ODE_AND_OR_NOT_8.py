56cec7010115ca168c0607cdd99400df90af2f25
blazdob <bldobr@gmail.com>
2021-01-06 17:45:41 +0100

    Correction on axes in logic gate simulations

--- cblb/run_ODE_AND_OR_NOT_8.py
+++ cblb/run_ODE_AND_OR_NOT_8.py
@@ -213,7 +213,7 @@ ax6.legend(['$y$ = out'])
 ax6.set_xlabel("Time [min]")
 ax6.set_ylabel("Concentrations [nM]")
 
-ticks = np.arange(0, 14, 1) * 1000
+ticks = np.arange(0, 28, 1) * 1000
 tick_labels = list(map(str, ticks))
 for i in range(len(tick_labels)):
     if i % 2 == 1:
@@ -229,6 +229,6 @@ ax6.set_xticklabels(tick_labels)
 
 # plt.suptitle("$out = \\overline{S}_1 \\overline{S}_0 I_0 \\vee \\overline{S}_1 S_0 I_1 \\vee S_1 \\overline{S}_0 I_2 \\vee S_1 S_0 I_3$")
 plt.gcf().set_size_inches(15, 10)
-plt.savefig("figs\\AND_OR_NOT_ode.pdf", bbox_inches='tight')
+plt.savefig("figs\\AND_OR_NOT_ode_8.pdf", bbox_inches='tight')
 
 plt.show()
